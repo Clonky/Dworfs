0db70ac0622b376593fb28585e365f9c5e7b978f
clonky_cs@yahoo.e <clonky_cs@yahoo.de>
2023-01-08 20:11:50 +0100

    Separate BehaviourMovement into separate files for GameComponents

---
+++ Assets/Scripts/Movement/Chasing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Chasing : BehaviourMovement
+{
+    public override void execute()
+    {
+        WalkTowards(GetDirectionToTarget(Target));
+    }
+    protected void WalkTowards(Vector2 directionToTarget)
+    {
+        rb.velocity += directionToTarget * Stats.Accel * Time.fixedDeltaTime;
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, Stats.Speed);
+    }
+
+    protected Vector2 GetDirectionToTarget(GameObject Target)
+    {
+        Vector2 directionToTarget = (Target.transform.position - transform.position).normalized;
+        return directionToTarget;
+    }
+}
---
+++ Assets/Scripts/Movement/StayAtRange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class StayAtRange: Chasing
+{
+    public override void execute()
+    {
+        if (GetRangeToTarget(Target) > Stats.PreferredRangeToTarget)
+        {
+            WalkTowards(GetDirectionToTarget(Target));
+        } else if (Mathf.Abs(GetRangeToTarget(Target) - Stats.PreferredRangeToTarget) < 0.1) {
+            rb.velocity = Vector3.zero;
+        } else
+        {
+            WalkTowards(-GetDirectionToTarget(Target));
+        }
+    }
+
+    float GetRangeToTarget(GameObject Target)
+    {
+        return (Target.transform.position - gameObject.transform.position).magnitude;
+    }
+}
