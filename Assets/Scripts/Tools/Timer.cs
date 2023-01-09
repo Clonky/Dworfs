76646d383e30869cb0a3d979b654d4140e6737ff
clonky_cs@yahoo.e <clonky_cs@yahoo.de>
2023-01-09 11:45:35 +0100

    Add a common place to store Timer functionality

---
+++ Assets/Scripts/Tools/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Tools
+{
+    public struct Timer
+    {
+        public Timer(float dur)
+        {
+            this.Duration = dur;
+            this.Elapsed = 0f;
+        }
+
+        public float Duration { get; }
+        public float Elapsed { get; set; }
+
+        public bool IsFinished()
+        {
+            if (Elapsed > Duration)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            this.Elapsed = 0f;
+        }
+    }
+}
